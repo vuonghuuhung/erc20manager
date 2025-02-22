6d8e79dbabf7b9a20863182ac74efd35a5c09525
vuonghuuhung <vuonghuuhung2002@gmail.com>
2025-02-22 12:15:23 +0700

    feat: add mock deployment script and update package configurations

--- packages/contracts/scripts/mock.ts
+++ packages/contracts/scripts/mock.ts
@@ -1,12 +1,8 @@
 import hre from "hardhat";
-import { ERC20Factory__factory } from "../typechain-types";
 
 const main = async () => {
-  const [account] = await hre.ethers.getSigners();
-  const erc20Factory = ERC20Factory__factory.connect(
-    "0x5fbdb2315678afecb367f032d93f642f64180aa3",
-    account
-  );
+  const Erc20Factory = await hre.ethers.getContractFactory("ERC20Factory");
+  const erc20Factory = await Erc20Factory.deploy();
 
   const res = await erc20Factory.mintERC20Manager("Test3", "TAT", 18, 10000000);
   console.log(res);
